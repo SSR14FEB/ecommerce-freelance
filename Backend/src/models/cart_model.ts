2738645319ecd4322a84b1fdaa399e2a27689caa
SSR14FEB <instaheri6@gmail.com>
2025-08-23 14:21:53 +0530

    feat: add cart and order models with schemas for managing cart items and orders

---
+++ Backend/src/models/cart_model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document } from "mongoose";
+import { Schema } from "mongoose";
+
+interface ItemInterface{
+    product_Id:mongoose.Types.ObjectId;
+    quantity:number;
+    price:number;
+}
+export interface cartInterface extends Document{
+    cartItem:ItemInterface[];
+    subTotal:number;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const cartSchema = new Schema<cartInterface>({
+    cartItem:{
+        type:[{
+            product_Id:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Product"
+            },
+            quantity:{
+                type:Number,
+                default:1,
+                min:1
+            },
+            price:{
+                type:Number,
+            }
+        }],default:[]
+    },
+    subTotal:{
+        type:Number,
+        default:0
+    }
+},{timestamps:true})
---
+++ Backend/src/models/order_models.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document } from "mongoose";
+import { Schema } from "mongoose";
+
+interface order {
+  product_Id: mongoose.Types.ObjectId;
+  quantity: number;
+  price: number;
+}
+
+export interface orderInterface extends Document {
+  ordered_Id: order[];
+  totalPrice: number;
+  createdAt: Date;
+  updateAt: Date;
+}
+
+const orderSchema = new Schema<orderInterface>(
+  {
+    ordered_Id: {
+      type: [
+        {
+          product_Id: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Product",
+          },
+          quantity: {
+            type: Number,
+            default: 1,
+            min:1,
+          },
+          price: {
+            type: Number,
+          },
+        },
+      ],default:[],
+    },
+    totalPrice:{
+        type:Number,
+        default:0,
+    }
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
