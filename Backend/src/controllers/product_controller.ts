64d2afe8b2d8c37240b5357f258a0b3c941fe92f
sonu <instaheri6@gmail.com>
2025-12-28 14:00:28 +0530

    refactor: migrate Redis configuration to a separate file and update imports

--- Backend/src/controllers/product_controller.ts
+++ Backend/src/controllers/product_controller.ts
@@ -6,7 +6,7 @@ import { Request,Response } from "express";
 import { IUserDocument } from "../types/models/user-model-types";
 import { createProduct, getProducts, getProductById, getProductByName, updateProduct, updateProductMedia} from "../services/product-service";
 import mongoose from "mongoose";
-// import {redis} from "../config/redis"
+import {redis} from "../config/redis-config"
 import { UpdateProductPayload, UpdateProductMediaPayload} from "../types/controllers/product-controller-type";
 
 
@@ -25,47 +25,47 @@ const createProductController = asyncHandler(async(req:Request, res:Response)=>{
 })
 
 const getAllProductsController = asyncHandler(async(req:Request, res:Response)=>{
-//     const {pageNo,sort,newestFirst} = req.query
-//     const pageNumber = parseInt(pageNo as string, 10) || 1
-//     const sortProducts = (parseInt(sort as string,10)=== -1?-1:1) as 1|-1
-//     const sortByProductDate = (parseInt(newestFirst as string,10)===-1?-1:1) as 1|-1
+    const {pageNo,sort,newestFirst} = req.query
+    const pageNumber = parseInt(pageNo as string, 10) || 1
+    const sortProducts = (parseInt(sort as string,10)=== -1?-1:1) as 1|-1
+    const sortByProductDate = (parseInt(newestFirst as string,10)===-1?-1:1) as 1|-1
     
-//     const cacheKey = `PageNumber:4{pageNo}:${sort}:${newestFirst}`
-//     const cachedPage = await redis.get(cacheKey)
-//     if(cachedPage){
-//         return res.status(200)
-//         .json(new ApiResponse(200,"product fetched successfully",true,JSON.parse(cachedPage)))
-//     }
-//     const products:Object = await getProducts(pageNumber, sortProducts, sortByProductDate)
-//     await redis.set(cacheKey,JSON.stringify(products),"EX",60)
-//     return res.status(200)
-//     .json(new ApiResponse(200,"product fetched successfully",true,products))
+    const cacheKey = `PageNumber:4{pageNo}:${sort}:${newestFirst}`
+    const cachedPage = await redis.get(cacheKey)
+    if(cachedPage){
+        return res.status(200)
+        .json(new ApiResponse(200,"product fetched successfully",true,JSON.parse(cachedPage)))
+    }
+    const products:Object = await getProducts(pageNumber, sortProducts, sortByProductDate)
+    await redis.set(cacheKey,JSON.stringify(products),"EX",60)
+    return res.status(200)
+    .json(new ApiResponse(200,"product fetched successfully",true,products))
 })
 
 const getProductByIdController = asyncHandler(async(req:Request, res:Response)=>{
-//     const {id}= req.params;
-//     const product:Object = await getProductById(id as string)
-//     return res.status(200)
-//     .json(new ApiResponse(200,"Product fetched successfully",true,product))
+    const {id}= req.params;
+    const product:Object = await getProductById(id as string)
+    return res.status(200)
+    .json(new ApiResponse(200,"Product fetched successfully",true,product))
 })
 
 const getProductByNameController = asyncHandler(async(req:Request, res:Response)=>{
-//      const {name,pageNo,sort,newestFirst} = req.query
-//      const pageNumber = parseInt(pageNo as string, 10) || 1
-//      const sortProducts = (parseInt(sort as string, 10)===-1?-1:1) as -1|1
-//      const sortByProductDate = (parseInt(sort as string, 10)===-1?-1:1) as -1|1
+     const {name,pageNo,sort,newestFirst} = req.query
+     const pageNumber = parseInt(pageNo as string, 10) || 1
+     const sortProducts = (parseInt(sort as string, 10)===-1?-1:1) as -1|1
+     const sortByProductDate = (parseInt(sort as string, 10)===-1?-1:1) as -1|1
 
-//      const cacheKey = `Product:${name}:${pageNo}:${sort}:${newestFirst}`
-//      const cachedProduct = await redis.get(cacheKey)
-//      if(cachedProduct){
-//         return res.status(200)
-//         .json(new ApiResponse(200,"Product fetched successfully",true,JSON.parse(cachedProduct)))
-//      }
+     const cacheKey = `Product:${name}:${pageNo}:${sort}:${newestFirst}`
+     const cachedProduct = await redis.get(cacheKey)
+     if(cachedProduct){
+        return res.status(200)
+        .json(new ApiResponse(200,"Product fetched successfully",true,JSON.parse(cachedProduct)))
+     }
 
-//      const product:Object = await getProductByName(name as string,pageNumber,sortProducts,sortByProductDate)
-//      await redis.set(cacheKey,JSON.stringify(product),"EX",60)
-//     return res.status(200)
-//     .json(new ApiResponse(200,"Product fetched successfully",true,product))
+     const product:Object = await getProductByName(name as string,pageNumber,sortProducts,sortByProductDate)
+     await redis.set(cacheKey,JSON.stringify(product),"EX",60)
+    return res.status(200)
+    .json(new ApiResponse(200,"Product fetched successfully",true,product))
 }) 
 
 const updateProductController = asyncHandler(async(req:Request<{},{},UpdateProductPayload>, res:Response)=>{
