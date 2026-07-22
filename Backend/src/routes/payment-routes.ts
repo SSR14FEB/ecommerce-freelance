f6e4a91fcd9cf85701b66d1ece6394d542859caf
sonu <instaheri6@gmail.com>
2026-07-22 16:28:38 +0530

    feat: add Jest testing framework and update payment routes with authentication middleware
    - Updated package.json to include Jest, Supertest, and TypeScript Jest preset.
    - Created jest.config.js for Jest configuration.
    - Added authentication middleware to payment routes for enhanced security.
    - Created new test files for various controllers to facilitate unit testing.
    - Added notification_controller.ts as a placeholder for future implementation.

--- Backend/src/routes/payment-routes.ts
+++ Backend/src/routes/payment-routes.ts
@@ -1,9 +1,10 @@
 import { Express, Router } from "express";
 import {createPaymentIntentController,verifyPaymentController,refundPaymentController, handlePaymentWebhookController} from "../controllers/payment_controller"
+import { authMiddleware } from "../middlewares/auth-middleware";
 
 const router = Router();
-router.post("/create-payment-intent/:cartId",createPaymentIntentController);
-router.post("/verify-payment",verifyPaymentController);
-router.post("/handle-webhook",handlePaymentWebhookController);
-router.post("/refund-payment/:orderId/:productId/:quantity/:reason",refundPaymentController)
+router.post("/create-payment-intent/:cartId",authMiddleware,createPaymentIntentController);
+router.post("/verify-payment",authMiddleware,verifyPaymentController);
+router.post("/handle-webhook",authMiddleware,handlePaymentWebhookController);
+router.post("/refund-payment/:orderId/:productId/:quantity/:reason",authMiddleware,refundPaymentController)
 export default router
